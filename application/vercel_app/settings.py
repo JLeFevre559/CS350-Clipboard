3152eca59fcf86036355658df4b037ffaa64e97b
joel <jlefevre559@gmail.com>
2023-09-18 23:26:12 -0600

    Static files fix v4.3

--- application/vercel_app/settings.py
+++ application/vercel_app/settings.py
@@ -124,6 +124,7 @@ STATIC_URL = '/static/'
 
 # Define the directory where your static files are located
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
