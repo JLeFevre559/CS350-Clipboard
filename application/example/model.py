cef1511857e1a14f5398055db24e829c4e90a174
joel <jlefevre559@gmail.com>
2023-09-23 17:24:26 -0600

    Models Update: description, priority, django user

--- application/example/model.py
+++ application/example/model.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.urls import reverse_lazy
+from django.contrib.auth.models import User
 
 class Project(models.Model):
     name = models.CharField(max_length=200)
+    description = models.TextField(default="None")
     
 class TaskList(models.Model):
     name = models.CharField(max_length=200)
@@ -20,8 +22,15 @@ class Tasks(models.Model):
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Not Started')
     due_date = models.DateField(blank=True, null=True)
     task_list = models.ForeignKey(TaskList, on_delete=models.CASCADE)
+    PRIORITY_CHOICES = (
+        ('High', 'High'),
+        ('Medium', 'Medium'),
+        ('Low', 'Low'),
+    )
+    priority = models.CharField(max_length=50, choices=PRIORITY_CHOICES, default='None')
 
 class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=1000, default="None")
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
     email = models.EmailField(max_length=200, default="None")
