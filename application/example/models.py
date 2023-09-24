7d815d8a9e7558c274323972a14f5d74c5952b04
joel <jlefevre559@gmail.com>
2023-09-23 21:34:03 -0600

    Update Profile to extend AbstractUser, and UUID

--- application/example/models.py
+++ application/example/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse_lazy
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+import uuid
 
 class Project(models.Model):
     name = models.CharField(max_length=200)
@@ -29,8 +30,8 @@ class Tasks(models.Model):
     )
     priority = models.CharField(max_length=50, choices=PRIORITY_CHOICES, default='None')
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class Profile(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     bio = models.TextField(max_length=1000, default="None")
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
     email = models.EmailField(max_length=200, default="None")
