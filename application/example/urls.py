caed9f0807365f06785f9917c253f6953c32ccc4
Alexander Hart <alexhart3000@gmail.com>
2023-09-27 14:47:25 -0600

    Url+View Update 9/27/2023

--- application/example/urls.py
+++ application/example/urls.py
@@ -1,7 +1,10 @@
 # example/urls.py
 from django.urls import path
-from example.views import Index
+from example.views import Index, Calendar, Project, Profile
 
 urlpatterns = [
     path('', Index.as_view(), name='Index'),
+    path('Calendar',Calendar.as_view(),name='Calendar'),
+    path('Project',Project.as_view(),name='Project'),
+    path('Profile',Profile.as_view(),name='Profile')
 ]
--- application/example/views.py
+++ application/example/views.py
@@ -6,19 +6,11 @@ from django.http import HttpResponse
 class Index(TemplateView):
     template_name = 'TempHome.html'
 
-'''Round two lets gooooooooo!!!!!!'''
+class Calendar(TemplateView):
+    template_name = 'Calendar.html'
 
-'''
-def index(request):
-    now = datetime.now()
-    html = f"""
-    <html>
-        <body>
-            <h1>Hello from Clipboard!</h1>
-            <h1>This is the hosted website for the TaskBoard team from UNCO for CS350</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    """
-    return HttpResponse(html)
-'''
+class Project(TemplateView):
+    template_name = 'Project.html'
+
+class Profile(TemplateView):
+    template_name = 'Profile.html'
